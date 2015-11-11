5dcbbc66b6efcf613c39c3860ff26a7aef1721d5
Byran Wills-Heath <byran@bluefruit.co.uk>
2015-11-11 00:08:19 +0000

    Moved the SourceMonitor conversion into a sub directory

--- SourceMonitor/AddCheckpointAndOutputMetrics.bat
+++ SourceMonitor/AddCheckpointAndOutputMetrics.bat
@@ -15,6 +15,6 @@ cd "%~dp0"
 "%LOCAL_PROGRAM_FILES%\SourceMonitor\SourceMonitor.exe" /s AddCheckpointAndExportMetrics.xml
 
 cd "%~dp0"
-xslconvert "Metrics.xml" MetricsToTeamCity.xslt Metrics.txt
+..\xslconvert "Metrics.xml" MetricsToTeamCity.xslt Metrics.txt
 
 type Metrics.txt
