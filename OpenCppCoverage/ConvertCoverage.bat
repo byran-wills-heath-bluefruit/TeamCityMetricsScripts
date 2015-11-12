3a73c13f167504832377faed3fe179b218ff1bbb
Byran Wills-Heath <byran@bluefruit.co.uk>
2015-11-12 08:52:47 +0000

    Removed multi parameters from ConvertCoverage as it wasn't working

--- OpenCppCoverage/ConvertCoverage.bat
+++ OpenCppCoverage/ConvertCoverage.bat
@@ -2,16 +2,8 @@
 
 set CURRENT_DIRECTORY="%~dp0"
 set SOURCES=%1
+set APP=%2
 
-set APP=
-:passing_parameters
-if "%2"=="" goto calculate_coverage
-set APP=%APP% %2
-shift
-goto passing_parameters
-
-
-:calculate_coverage
 cd "%CURRENT_DIRECTORY%"
 del Coverage.xml
 opencppcoverage --sources %SOURCES% --export_type=cobertura:Coverage.xml -- %APP%
