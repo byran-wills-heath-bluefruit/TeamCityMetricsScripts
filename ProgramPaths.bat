083a2e6ec3766fc14db8602d504f1559a1e57077
Byran Wills-Heath <byran@bluefruit.co.uk>
2015-12-07 10:15:30 +0000

    Added script that sets program paths

---
+++ ProgramPaths.bat
@@ -0,0 +1,19 @@
+@echo off
+
+IF DEFINED ProgramFiles(x86) GOTO :64Bit
+
+SET PROGRAM_FILES_32BIT=%ProgramFiles%
+SET PROGRAM_FILES_64BIT=%ProgramFiles%
+GOTO :ProgramFilesSet
+
+:64Bit
+SET PROGRAM_FILES_32BIT=%ProgramFiles(x86)%
+SET PROGRAM_FILES_64BIT=%ProgramFiles%
+
+:ProgramFilesSet
+
+set SOURCEMONITOR="%PROGRAM_FILES_32BIT%\SourceMonitor\SourceMonitor.exe"
+set VSTEST_2013="%PROGRAM_FILES_32BIT%\Microsoft Visual Studio 12.0\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe"
+set DEVENV_2013="%PROGRAM_FILES_32BIT%\Microsoft Visual Studio 12.0\Common7\IDE\devenv.exe"
+set OPENCOVER="%PROGRAM_FILES_32BIT%\OpenCover\OpenCover.Console.exe"
+set SEVENZIP="%PROGRAM_FILES_64BIT%\7-Zip\7z.exe"
